a65a52946fcc761903e99ebdccb9b28fc4dc8b9e
David <davidkvelderman@gmail.com>
2020-02-12 14:09:29 +0100

    Opdracht 2.1

---
+++ Hoofdstuk 2/opdracht_2-1.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<?php
+/**
+ * User: David S. Klein Velderman
+ * Date: Date
+ * Time: 12:06 PM
+ * File: index.php
+ */
+?>
+
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+
+<aside>
+    <h2>
+        Menu
+    </h2>
+
+    <ul>
+        <li>
+            Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+
+<main id="wrapper">
+    <h2>
+        Uitwerkingen
+    </h2>
+</main>
+
+</body>
+
+
+</html>
+
