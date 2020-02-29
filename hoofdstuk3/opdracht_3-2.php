8c710a576584dfe1d04cb2e81e4bcd66b7aaf419
David <davidkvelderman@gmail.com>
2020-02-29 13:50:15 +0100

    Opdracht 3.2, taak 3

--- hoofdstuk3/opdracht_3-2.php
+++ hoofdstuk3/opdracht_3-2.php
@@ -1,19 +1,61 @@
-<link rel="stylesheet" href="3-2.css">
-
 <?php
 
-    $trafficLightColor  = "groen";
-    $ambulanceComing    = false;
-    $driveOn            = true;
+    // Maak klasse Country
+    class Country {
+        // Public gedaan om daarom
+        public $name;
+        public $lowAge;
+        public $highAge;
 
-    if ($trafficLightColor == "rood" || $trafficLightColor == "oranje") {
-        $driveOn = false;
-    } else if ($ambulanceComing) {
-        $driveOn = false;
+        // Constructor
+        public function __construct($name, $lowAlcAge, $highAlcAge)
+        {
+            $this->name = $name;
+            $this->lowAge = $lowAlcAge;
+            $this->highAge = $highAlcAge;
+        }
     }
 
-    if ($driveOn) {
-        echo "<p class=\"green\"> U mag doorrijden </p>";
-    } else {
-        echo "<p class=\"red\"> U moet stoppen </p>";
+    // Instanciëer alle landen
+    $belg = new Country("België", 16, 18);
+    $bulg = new Country("Bulgarijë", 18, 18);
+    $cyp = new Country("Cyprus", 17, 17);
+    $ned = new Country("Nederland", 18, 18);
+    $zwe = new Country("Zweden", 18, 20);
+
+    // Maak array met alle landen
+    $countries = [
+        $belg, $bulg, $cyp, $ned, $zwe
+    ];
+
+    // De twee verplichte variabelen
+    $countryName = "Bulgarijë";
+    $currentAge  = 18;
+
+    // Variabele voor straks, om een country in te stoppen.
+    // Voor nu is hij null, want dan zeurt de compiler niet.
+    $currentCountry = null;
+
+    // For-loop die een land uit de array vist met dezelfde naam als countryName
+    for ($i = 0; $i < count($countries); $i++) {
+
+        if ($countryName == $countries[$i]->name) {
+            $currentCountry = $countries[$i];
+            break;
+        }
+    }
+
+    echo "<p>Je woont in " . $currentCountry->name . " en je bent " . $currentAge . " jaar oud.";
+
+    // Als je leeftijd hoger zit dan de leeftijd voor sterke drank,
+    // hoef je niet voor alle andere condities te checken.
+    // Vandaar dat hij dus bovenaan staat.
+    if ($currentAge >= $currentCountry->highAge){
+        echo "<p>Je mag hier alle drank drinken.</p>";
+    }
+    elseif ($currentAge >= $currentCountry->lowAge && $currentAge < $currentCountry->highAge) {
+        echo "<p>Je mag hier alleen zwakke drank drinken.</p>";
+    }
+    else {
+        echo "<p>Je mag hier niets drinken.</p>";
     }
